c5575787986663ff2c9929b570c492bf07b4cff5
Eugene-kms <ievgenkolomiiets@gmail.com>
2024-09-05 22:03:08 +0200

    Added tests for MoviesViewModel, MovieDetailVM, MBMocks, MBTests

--- MBMoviesTests/MBMoviesTests.swift
+++ MBMoviesTests/MBMoviesTests.swift
@@ -1,14 +1,13 @@
 //
-//  MoviesBrowserTests.swift
-//  MoviesBrowserTests
+//  MBMoviesTests.swift
+//  MBMoviesTests
 //
-//  Created by Eugene on 22.08.2024.
+//  Created by Eugene on 05.09.2024.
 //
 
 import XCTest
-@testable import MoviesBrowser
 
-final class MoviesBrowserTests: XCTestCase {
+final class MBMoviesTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -28,7 +27,7 @@ final class MoviesBrowserTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
